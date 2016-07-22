a257f48f4908e322de84f6a305404b1ce8481b06
RafaelAPimentel <Rafael.Pimentel@newbury.edu>
2016-07-22 14:00:50 -0400

    uploading jsp example

---
+++ JSP/build/web/response.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : newjsp
+    Created on : Jul 22, 2016, 1:33:47 PM
+    Author     : Guido
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <jsp:useBean id="mybean" scope="session" class="org.mypackage.hello.NameHandler" />
+        <jsp:setProperty name="mybean" property="name" />
+        <h1>Hello, <jsp:getProperty name="mybean" property="name"/>!</h1>
+    </body>
+</html>
---
+++ JSP/src/java/NameHandler.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.hello;
+/**
+ *
+ * @author Guido
+ */
+public class NameHandler {
+    private String name;
+    public NameHandler(){
+        name =null;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
